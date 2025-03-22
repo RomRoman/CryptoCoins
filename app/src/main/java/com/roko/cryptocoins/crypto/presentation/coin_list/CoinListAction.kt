e5985c17df6b44d003ccc8603d6d83f701d8dc86
romka <r.romanenko@uplata.com>
2025-03-22 23:02:13 +0000

    Coin list Screen:  - Implemented Coins list request;

--- app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListAction.kt
+++ app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListAction.kt
@@ -1,5 +1,8 @@
 package com.roko.cryptocoins.crypto.presentation.coin_list
 
-sealed interface CoinListAction {
+import com.roko.cryptocoins.crypto.presentation.models.CoinUi
 
+sealed interface CoinListAction {
+    data class OnCoinClick(val coinUi: CoinUi) : CoinListAction
+    data object OnRefresh: CoinListAction
 }
--- app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -1,10 +1,59 @@
 package com.roko.cryptocoins.crypto.presentation.coin_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.roko.cryptocoins.core.domain.util.onFailure
+import com.roko.cryptocoins.core.domain.util.onSuccess
+import com.roko.cryptocoins.crypto.domain.CoinDataSource
+import com.roko.cryptocoins.crypto.presentation.mappers.toCoinUi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class CoinListViewModel: ViewModel() {
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+): ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state = _state
+        .onStart {
+            loadCoins()
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            CoinListState()
+        )
+
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            coinDataSource
+                .getCoins()
+                .onSuccess { coins ->
+                    _state.update { it.copy(
+                        isLoading = false,
+                        coins = coins.map { coin -> coin.toCoinUi() }
+                    ) }
+                }
+                .onFailure { error ->
+                    _state.update { it.copy(isLoading = false) }
+                }
+        }
+    }
 
     fun onAction(action: CoinListAction) {
+        when(action) {
+            is CoinListAction.OnCoinClick -> {
+
+            }
+            CoinListAction.OnRefresh -> {
+                loadCoins()
+            }
+        }
 
     }
 
