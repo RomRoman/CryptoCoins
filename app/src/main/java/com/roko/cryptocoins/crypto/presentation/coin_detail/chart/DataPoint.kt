e5aa3cb9a9455d1e170df62924cc13eafc170a5b
romka <r.romanenko@uplata.com>
2025-03-25 16:23:52 +0000

    Line Chart:  - Created Line chart view  - Show Coin history on CoinDetailScreen

---
+++ app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_detail/chart/DataPoint.kt
@@ -0,0 +1,7 @@
+package com.roko.cryptocoins.crypto.presentation.coin_detail.chart
+
+data class DataPoint(
+    val x: Float,
+    val y: Float,
+    val xLabel: String,
+)
--- app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/roko/cryptocoins/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.roko.cryptocoins.core.domain.util.onFailure
 import com.roko.cryptocoins.core.domain.util.onSuccess
 import com.roko.cryptocoins.crypto.domain.CoinDataSource
+import com.roko.cryptocoins.crypto.presentation.coin_detail.chart.DataPoint
 import com.roko.cryptocoins.crypto.presentation.mappers.toCoinUi
 import com.roko.cryptocoins.crypto.presentation.models.CoinUi
 import kotlinx.coroutines.channels.Channel
@@ -16,6 +17,7 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
@@ -78,7 +80,25 @@ class CoinListViewModel(
                     end = ZonedDateTime.now()
                 )
                 .onSuccess { history ->
-                    println(history)
+                    val dataPoints = history
+                        .sortedBy { it.dateTime }
+                        .map {
+                            DataPoint(
+                                x = it.dateTime.hour.toFloat(),
+                                y = it.priceUsd.toFloat(),
+                                xLabel = DateTimeFormatter
+                                    .ofPattern("ha\nM/d")
+                                    .format(it.dateTime)
+                            )
+                        }
+
+                    _state.update {
+                        it.copy(
+                            selectedCoin = it.selectedCoin?.copy(
+                                coinPriceHistory = dataPoints
+                            ),
+                        )
+                    }
                 }
                 .onFailure { error ->
                     _events.send(CoinListEvent.Error(error))
